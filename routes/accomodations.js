45a20d28dd55db33cf082cb0523f454fb0e7dcff
KB-Toast <kevin.brusson@gmail.com>
2024-03-06 11:50:05 +0100

    changed params id to body id

--- routes/accomodations.js
+++ routes/accomodations.js
@@ -4,30 +4,47 @@ require('../models/connexion');
 const Trip = require('../models/trips');
 const { checkBody } = require('../modules/checkBody');
 
-// GET all accomodations within a trip
-router.get('/:id', (req, res) => {
-    Trip.findById(req.params.id).populate('accomodations').then(data => {
+// "GET" all accomodations within a trip by body.id
+router.post('/', (req, res) => {
+    Trip.findById(req.body.id).populate('accomodations').then(data => {
         res.json({result: true, data: data.accomodations});
       });
 });
 
 // UPDATE to ADD a new accomodation in a trip
-router.post('/:id/new', (req, res) => {
+router.post('/new', (req, res) => {
     // checks fields
-    if (!checkBody(req.body, ['name', 'location', 'date' ])) {
+    if (!checkBody(req.body, ['name', 'date'])) {
         res.json({ result: false, error: 'Missing or empty fields' });
         return;
     }
 
-    const newAccomodation = ({
-        name: req.body.name,
-        location: req.body.location,
-        date: req.body.date
+    // checks if name is already taken for this accomodation
+    Trip.findOne({ accomodations: { $elemMatch: {name: {$regex: new RegExp(req.body.name, 'i')} } } }).then(data => {
+        console.log('trying to find accomodation : ' + data);
+        if (data !== null) {
+            console.log('name taken soz');
+            res.json({ result: false, error: 'Accomodation name already taken'})
+        } else {
+            console.log('name open yay');
+            const newAccomodation = ({
+                name: req.body.name,
+                location: req.body.location,
+                date: req.body.date,
+                picture: req.body.picture,
+                url: req.body.url,
+                description: req.body.description,
+                budget: req.body.budget,
+                vote: [],
+                isFixed: false,
+            });
+        
+            Trip.updateOne({_id: req.body.tripId}, { $push: { accomodations: newAccomodation}}).then(data => {
+                console.log('trip found');
+                res.json({result: true, data: data});
+              });
+        }
     });
-
-    Trip.updateOne({_id : req.params.id}, { $push: { accomodations: newAccomodation}}).then(data => {
-        res.json({result: true, data: data});
-      });
 });
 
 
