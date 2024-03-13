21690791075c44a54da0292dae27504a3f2fc539
KB-Toast <kevin.brusson@gmail.com>
2024-03-13 16:15:52 +0100

    changing accomodation logic to dispatch correctly

--- routes/accomodations.js
+++ routes/accomodations.js
@@ -58,9 +58,11 @@ router.post('/new', (req, res) => {
                     Trip.updateOne({_id: req.body.tripId}, { $inc: { budget: req.body.budget}}).then(data => {  
                     });
                 }
-                //res.json({result: true, data: data, message: 'Logement ajouté avec succès !'});
-                res.json({result: true, newAccomodation, message: 'Logement ajouté avec succès !'});
-              });
+                Trip.findOne({ accomodations: { $elemMatch: {name: {$regex: new RegExp(req.body.name, 'i')} } } })
+                .then(data => {
+                    res.json({result: true, newAccomodation: data, message: 'Logement ajouté avec succès !'});
+                });
+            });
         }
     });
 });
